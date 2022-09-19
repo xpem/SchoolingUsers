9384b9b318cc16da3c78de4d2e8c8456739bc7aa
emanuel <emanuel@enotas.com.br>
2022-09-19 09:31:13 -0300

    first commit

---
+++ Domain/Services/UserService.cs
@@ -0,0 +1,86 @@
+﻿using Models;
+using Repositories.Contexts;
+using Repositories.UserRepository;
+
+namespace Domain.Services
+{
+    public class UserService : IUserService
+    {
+
+        #region Property
+        private IUserRepository<User> UserRepository;
+        private UserContext UserContext;
+        #endregion
+
+
+        #region Constructor
+        public UserService(IUserRepository<User> userRepository, UserContext userContext)
+        {
+            UserRepository = userRepository;
+            UserContext = userContext;
+        }
+
+        #endregion
+
+        public Result AddUser(User user)
+        {
+            Result result = ValidateUser(user);
+
+            if (result.Success)
+                UserRepository.Insert(user);
+            else return result;
+
+            return new Result() { Success = true };
+        }
+
+        public static Result ValidateUser(User user)
+        {
+            if (user.BirthDate > DateTime.Now)
+            {
+                return new Result() { Success = false, Message = "Birth date can't be a future date." };
+            }
+
+            if (user.Email == null) return new Result() { Success = false, Message = "Email is Required" };
+
+
+            if (!Utils.ValidateEmail.IsValidEmail(user.Email))
+            {
+                return new Result() { Success = false, Message = "Invalid Email" };
+            }
+
+            if (user.SchoolingId < 0 || user.SchoolingId > 2)
+            {
+                return new Result() { Success = false, Message = "Invalid SchoolingId" };
+            }
+
+            return new Result() { Success = true };
+        }
+
+        public void Delete(int id)
+        {
+            var user = GetUser(id);
+
+            if (user == null) return;
+
+            UserRepository.Delete(user);
+        }
+
+        public User? GetUser(int id) { User? user = UserContext.Users?.FirstOrDefault(c => c.Id == id); return user; }
+
+        public List<User> GetUsers() => UserRepository.GetAll();
+
+        public Result UpdateUser(User user)
+        {
+            Result result = ValidateUser(user);
+
+            if (result.Success)
+            {
+                UserContext.ChangeTracker.Clear();
+                UserRepository.Update(user);
+            }
+            else return result;
+
+            return new Result() { Success = true };
+        }
+    }
+}
---
+++ SchoolingUsers/Controllers/UserController.cs
@@ -0,0 +1,107 @@
+﻿
+using Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using SchoolingUsers.DTO;
+
+
+namespace SchoolingUsers.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService UserService;
+
+        public UserController(IUserService userService)
+        {
+            UserService = userService;
+        }
+
+
+        [HttpPost(Name = "PostUser")]
+        public ActionResult Post([FromBody] UserDTO userDTO)
+        {
+            try
+            {
+                if (userDTO == null) return NotFound();
+                
+                User user = new() { Name = userDTO.Name, LastName = userDTO.LastName, Email = userDTO.Email, BirthDate = userDTO.BirthDate, SchoolingId = userDTO.SchoolingId };
+
+               Result result =  UserService.AddUser(user);
+
+                if (!result.Success)
+                {
+                    return ValidationProblem(result.Message);
+                }
+
+                return Ok("User Added");
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPut(Name = "PutUser")]
+        public ActionResult Put([FromBody] UserDTO userDTO)
+        {
+            try
+            {
+                if (userDTO == null) return NotFound();
+                if (userDTO.Id == null) return NotFound();
+
+                User? user = UserService.GetUser(userDTO.Id.Value);
+
+                if (user == null) return NotFound();
+
+                user = new() { Id = userDTO.Id.Value, Name = userDTO.Name, LastName = userDTO.LastName, Email = userDTO.Email, BirthDate = userDTO.BirthDate, SchoolingId = userDTO.SchoolingId, HistoricSchooling = user.SchoolingId };
+
+                Result result = UserService.UpdateUser(user);
+
+                if (!result.Success)
+                {
+                    return ValidationProblem(result.Message);
+                }
+
+                return Ok("User Updated");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        [HttpGet(Name = "GetUsers")]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            return UserService.GetUsers();
+        }
+
+        [HttpGet("{id}", Name = "GetUser")]
+        public ActionResult<User> Get(int id)
+        {
+            var user = UserService.GetUser(id);
+
+            if (user == null) return NotFound();
+
+            return Ok(user);
+        }
+
+        [HttpDelete(Name = "DeleteUser")]
+        public void Delete(int id)
+        {
+            try
+            {
+                UserService.Delete(id);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
